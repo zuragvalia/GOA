6c8fd6ae0b8dfb519c1aa1614a8ef6875382d001
zuragvalia <gvalia2011@gmail.com>
2024-05-23 22:26:52 +0400

    Add files via upload

---
+++ day1/level1.py
@@ -0,0 +1,34 @@
+name = "nika"
+#name  არის ცვლადი
+# = არის ცვლადის მნიშვნელობის მიმნიჯებელი სინბოლო
+# "nika" არისცვლადისთვის მნიშვნელობა
+
+surname = "keshelava"
+
+print(name)
+#პრინტ პუნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ან სტრინგი
+
+name = "nika" #ეს არის str(string) ტიპის ცვლადი
+age = 22 # ეს არის int (integer) მთელი რიცხვი
+height = 184.5 #ეს არისfloat ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming =True   #True or false
+is_ugly = False   #snakecase(უბრალოდ წერის სტილი სატანდარტულად)
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+
+print(name +" "+ surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name +age)
+
+#print(type(age)
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " +str(age))
+
