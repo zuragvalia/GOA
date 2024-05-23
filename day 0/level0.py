14e0ab5645b014fb08bf2cb71b10545009ca8608
zuragvalia <gvalia2011@gmail.com>
2024-05-23 22:25:58 +0400

    Add files via upload

---
+++ day 0/level0.py
@@ -0,0 +1,49 @@
+from turtle import*
+
+
+#we want to paint a house
+
+#step 1:  draw a square
+width(7)
+color("pink")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of squre
+
+#drawing a door
+
+forward(70)
+color("pink")
+left(90)
+forward(120) #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+penup()
+goto(200, 200)
+pendown()
+
+
+
+color("pink")
+right(150)
+forward(200)
+right(240)
+forward(200)
+
+
+
+
+
+exitonclick()
